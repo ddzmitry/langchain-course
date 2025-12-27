7b1329734a6fcf2a774ff94b0a51b2696f78244e
Dzmitry Dubarau <dzmitry.dubarau@gmail.com>
2025-12-27 18:51:56 -0500

    updated folder name

--- rag_pinecone_langchain/ingestion.py
+++ rag_pinecone_langchain/ingestion.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 if __name__ == "__main__":
     print("Ingesting...")
-    loader = TextLoader("./rag_pinecone/mediumblog1.txt")
+    loader = TextLoader("./rag_pinecone_langchain/mediumblog1.txt")
     document = loader.load()
 
     print("splitting...")
